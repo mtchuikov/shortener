21c1a80bf7c82f7161b21ca26543d033709c6b1b
mtchuikov <mtchuikov@yandex.ru>
2025-03-03 23:57:51 +0300

    add env config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,13 +1,23 @@
 package config
 
+import (
+	"github.com/caarlos0/env/v11"
+)
+
 type Config struct {
-	Addr    string
-	BaseURL string
-	Verbose bool
+	Addr    string `env:"SERVER_ADDRESS"`
+	BaseURL string `env:"BASE_URL"`
+	Verbose bool   `env:"VERBOSE"`
 }
 
-func New() Config {
+func New() (Config, error) {
 	config := Config{}
 	loadFromFlags(&config)
-	return config
+
+	err := env.Parse(&config)
+	if err != nil {
+		return Config{}, err
+	}
+
+	return config, nil
 }
