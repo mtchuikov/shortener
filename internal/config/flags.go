8b9aaa46e9430a655ce7efb05bf3b261292521f9
mtchuikov <mtchuikov@yandex.ru>
2025-03-03 22:34:42 +0300

    fix errors

--- internal/config/flags.go
+++ internal/config/flags.go
@@ -3,7 +3,7 @@ package config
 import "github.com/spf13/pflag"
 
 var (
-	addr    = pflag.StringP("addr", "a", "127.0.0.1:8080", "server address")
+	addr    = pflag.StringP("addr", "a", "127.0.0.1:8080/", "server address")
 	baseURL = pflag.StringP("base", "b", "", "base url for shortened links")
 	verbose = pflag.BoolP("verbose", "v", false, "print verbose logs")
 )
