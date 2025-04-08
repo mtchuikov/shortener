1eab64d75eecf79f0f8e8e24b705024d4c58021e
mtchuikov <mtchuikov@yandex.ru>
2025-04-08 22:07:52 +0300

    fix errors

--- internal/services/resolver.go
+++ internal/services/resolver.go
@@ -2,7 +2,6 @@ package services
 
 import (
 	"context"
-	"regexp"
 )
 
 type resolverRepo interface {
@@ -17,22 +16,6 @@ func NewResolver(repo resolverRepo) *resolverService {
 	return &resolverService{repo}
 }
 
-var shortIDRegexp = regexp.MustCompile(`^[A-Za-z0-9]{8}$`)
-
-func (s *resolverService) validateShortID(id string) error {
-	valid := shortIDRegexp.MatchString(id)
-	if !valid {
-		return ErrInvalidShortID
-	}
-
-	return nil
-}
-
 func (s *resolverService) Serve(ctx context.Context, shortID string) (string, error) {
-	err := s.validateShortID(shortID)
-	if err != nil {
-		return "", err
-	}
-
 	return s.repo.GetOriginalURL(ctx, shortID)
 }
